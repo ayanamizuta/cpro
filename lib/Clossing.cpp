a8dff5e8d4c9f7689b6300ae8e487940edcbe26a
Mizuta Rei <mizuta1018@gmail.com>
2018-09-22 09:07:25 +0900

    Add

--- lib/Clossing.cpp
+++ lib/Clossing.cpp
@@ -1,46 +1,71 @@
-//return the number of indices s.t. i<j and v[j]<v[i]
-int Clossing(vector<LL> v){
-  int v_len=v.size();
+//return the number of indices s.t. i<j and gt(v[j],v[i])
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+using namespace std;
+
+template <typename T>
+long long Clossing(std::vector<T> v, bool(*gt)(T,T)){
+  long long v_len=(long long)v.size();
   if(v_len>1){
-    int v1_len = v_len/2;
-    int v2_len = v_len-v1_len;
-    vector<LL> v1,v2;
-    REP(i,v_len){
-      if(i<v1_len)
-	v1.push_back(v[i]);
-      else
-	v2.push_back(v[i]);
-    }
-    int ret1=tentou(v1);
-    sort(v1.begin(),v1.end());
-    int ret2=tentou(v2);
-    sort(v2.begin(),v2.end());
-    int ind=0,su=0;
-    REP(i,v1_len){
+    long long v1_len = v_len/2;
+    long long v2_len = v_len-v1_len;
+    std::vector<T> v1,v2;
+    copy(v.begin(), v.begin()+v1_len, back_inserter(v1));
+    copy(v.begin()+v1_len, v.end(), back_inserter(v2));
+    long long ret1=Clossing(v1,gt);
+    std::sort(v1.begin(),v1.end(),gt);
+    long long ret2=Clossing(v2,gt);
+    std::sort(v2.begin(),v2.end(),gt);
+    int idx2=0;
+    long long acm=0;
+    for(int idx1=0;idx1<v1_len;idx1++){
       while(true){
-	if(v1[i]<=v2[ind] || ind==v2_len-1){
-	  su+=ind;
-	  if(v1[i]>v2[v2_len-1])
-	    su++;
+	if(!gt(v2[idx2],v1[idx1]) || idx2==v2_len-1){
+	  acm+=idx2;
+	  if(gt(v2[v2_len-1],v1[idx1]))
+	    acm++;
 	  break;
 	}
-	ind++;
+	idx2++;
       }
     }
-    return su+ret1+ret2;
+    return acm+ret1+ret2;
   }
   return 0;
 }
 
+
+/*
+  --------demo--------
+ */
+
+bool gt(long long l1,long long l2){
+  return l1<l2;
+}
+
+bool gte(long long l1,long long l2){
+  return l1<=l2;
+}
+
+
 int main(){
-  vector<LL>v;
+  std::vector<long long>v;
   int n;
   cin>>n;
-  LL a;
-  REP(i,n){
+  long long a;
+  for(int i=0;i<n;i++){
     cin>>a;
     v.push_back(a);
   }
-  cout<<tentou(v)<<endl;
+  cout<<"gt:"<<Clossing(v,gt)<<endl;
+  cout<<"gte:"<<Clossing(v,gte)<<endl;
+
+  std::vector<long long>v_big;
+  for(int i=0;i<100000;i++){
+    v_big.push_back(1000000-i);
+  }
+  cout<<"gt:"<<Clossing(v_big,gt)<<endl;
   return 0;
 }
