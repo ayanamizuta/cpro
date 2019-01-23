561f286b2d59f8e17ad7e71ec9464c4808910cfb
Mizuta Rei <mizuta1018@gmail.com>
2019-01-23 22:40:41 +0900

    Add atcoder/joi/2010/a.out.dSYM/Contents/Info.plist atcoder/joi/2010/e.cpp atcoder/joi/e.cpp lib/SegmentTree.cpp

--- atcoder/joi/2010/e.cpp
+++ atcoder/joi/2010/e.cpp
@@ -1,67 +1,10 @@
-#include <functional>
+#include "bits/stdc++.h"
 using namespace std;
 
-// SegmentTree
-// updating with max (long long)
-const int MAXSEGN = 100010;
-class SegmentTree{
-public:
-  int size=0;
-  int depth=0; // the depth of the tree
-  int best=0;  // the index of maximum element
-  int start=0; // the initial index of inputs
-  long long dat[4 * MAXSEGN];
-  
-  SegmentTree(int n,T* v){
-    size=n;
-    while((1<<depth)<n)depth++;
-    start=(1<<depth)-1;
-    for(int i=start;i<(1<<(depth+1));i++){
-      if(i<start+n)
-	dat[i]=v[i-start];
-      else
-	dat[i]=-1e15;
-    }
-    for(int i=start;i<start+n;i++){
-      int tmp=i;
-      while(par(tmp)!=-1){
-	dat[par(tmp)]=max(dat[tmp],dat[(tmp+1)/2*4-1-tmp]);
-	tmp=par(tmp);
-      }
-    }
-    int i=0;
-    while(i<start)i=(dat[i]==dat[2*i+1])?2*i+1:2*i+2;
-    best=i-start;
-  }
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)   FOR(i,0,n)
+#define LL long long
 
-  int par(int i){
-    if(i==0)
-      return -1;
-    return (i-1)/2;
-  }
-
-  void update(int d,long long x){
-    int i=d+start;
-    dat[i]=x;
-    while(par(i)!=-1){
-      dat[par(i)]=max(dat[i],dat[(i+1)/2*4-1-i]);
-      i=par(i);
-    }
-    i=0;
-    while(i<start){
-      i=(dat[i]==dat[2*i+1])?2*i+1:2*i+2;
-    }
-    best=i-start;
-  }
-
-  long long max_element(){
-    return dat[best];
-  }
-  
-};
-
-//StarrySky Tree
-// intarval update & max
 const int MAXSSTN = 100010;
 class StarrySkyTree{
 public:
@@ -124,9 +67,17 @@ public:
     best=i-start;
   }
   void dump(){
-    for(int i=0;i<depth;i++){
+    cerr<<"SegTree:"<<endl;
+    for(int i=0;i<depth+1;i++){
       for(int j=0;j<(1<<i);j++)
 	cerr<<dat[j+(1<<i)-1]<<" ";
+      cerr<<endl;
+    }
+    cerr<<endl;
+    cerr<<"SegTree lazy addition:"<<endl;
+    for(int i=0;i<depth+1;i++){
+      for(int j=0;j<(1<<i);j++)
+	cerr<<dat_add[j+(1<<i)-1]<<" ";
       cerr<<endl;
     }
     cerr<<endl;
@@ -152,8 +103,63 @@ private:
   }
 };
 
+int n;
+LL H;
+LL d[100000],h[100000];
+
+struct cost{
+  int ind;
+  LL val;
+};
+
+bool operator<(const cost &c1,const cost &c2){
+  return c1.val<c2.val;
+}
 
 int main(){
-  
+  cin>>n>>H;
+  vector<LL> heal(n,0);
+  REP(i,n-1)
+    cin>>d[i]>>h[i];
+  StarrySkyTree sst(n);
+  priority_queue<cost> que;
+  LL ret=0;
+  LL l=H;
+  REP(i,n-1){
+    sst.update(i,l);
+    cost c={i,min(h[i],H-l)};
+    que.push(c);
+    l-=d[i];
+    if(l>0)continue;
+    else{
+      while(l<=0){
+	cost c = que.top();
+	LL maxl = sst.max_element(c.ind,i+1);
+	if(c.val > H-maxl){
+	  que.pop();
+	  c.val=H-maxl;
+	  que.push(c);
+	}else{
+	  LL num = min((H-maxl)/c.val,1+(-l)/c.val);
+	  sst.add(c.ind,i+1,c.val*num);
+	  l+=c.val*num;
+	  ret+=num;
+	  heal[c.ind]+=num; 
+	}
+      }
+    }
+  }
+  /*
+  l=H;
+  REP(i,n-1){
+    l+=heal[i]*h[i];
+    l=min(l,H);
+    l-=d[i];
+    if(l<=0){
+      cerr<<i<<":0 life"<<endl;
+    }
+  }
+  */
+  cout<<ret<<endl;
   return 0;
 }
